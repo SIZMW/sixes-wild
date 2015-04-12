d37b49b878758f57037d4b42600c65607f98c010
Aditya <adityanivarthi19@gmail.com>
2015-04-12 13:44:03 -0400

    Commented and added get() methods on TopMenuPanel. Modified layout to better display the text field.

--- src/main/java/SixesWild/com/mimas/sixeswild/boundaries/TopMenuPanel.java
+++ src/main/java/SixesWild/com/mimas/sixeswild/boundaries/TopMenuPanel.java
@@ -1,38 +1,51 @@
 package SixesWild.com.mimas.sixeswild.boundaries;
-import javax.swing.JPanel;
-import java.awt.GridBagLayout;
-import javax.swing.JTextArea;
-import java.awt.GridBagConstraints;
-import javax.swing.JButton;
-import java.awt.Insets;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 
+/**
+ * @author Yahel Nachum
+ * @brief This class represents the top panel in the level builder.
+ */
 public class TopMenuPanel extends JPanel {
+	
+	final String MOVE_TEXT = "Move ";
+	JTextField levelNameTextField;
+	JButton specialMoveButton1;
+	JButton specialMoveButton2;
+	JButton specialMoveButton3;
+	JButton specialMoveButton4;
 
 	/**
-	 * Create the panel.
+	 * @brief Constructor for the TopMenuPanel class.
 	 */
 	public TopMenuPanel() {
 		GridBagLayout gridBagLayout = new GridBagLayout();
 		gridBagLayout.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0};
-		gridBagLayout.columnWeights = new double[]{1.0, 60.0, 20.0, 5.0, 1.0, 5.0, 1.0, 5.0, 1.0, 5.0, 1.0, Double.MIN_VALUE};
+		gridBagLayout.columnWeights = new double[]{1.0, 100.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, Double.MIN_VALUE};
 		gridBagLayout.rowWeights = new double[]{1.0, 5.0, 1.0, Double.MIN_VALUE};
 		setLayout(gridBagLayout);
 		
-		JTextArea levelNameText = new JTextArea();
-		levelNameText.setEditable(false);
+		// Level name text 
+		levelNameTextField = new JTextField();
+		levelNameTextField.setEditable(false);
 		GridBagConstraints gbc_levelNameText = new GridBagConstraints();
 		gbc_levelNameText.insets = new Insets(0, 0, 5, 5);
-		gbc_levelNameText.fill = GridBagConstraints.BOTH;
+		gbc_levelNameText.fill = GridBagConstraints.HORIZONTAL;
 		gbc_levelNameText.gridx = 1;
 		gbc_levelNameText.gridy = 1;
-		add(levelNameText, gbc_levelNameText);
+		add(levelNameTextField, gbc_levelNameText);
 		
-		JButton specialMoveButton1 = new JButton("SpecialMoveButton1");
+		// Special move button 1
+		specialMoveButton1 = new JButton(MOVE_TEXT + 1);
 		specialMoveButton1.setPreferredSize(new Dimension(100, 100));
 		GridBagConstraints gbc_specialMoveButton1 = new GridBagConstraints();
 		gbc_specialMoveButton1.insets = new Insets(0, 0, 5, 5);
@@ -40,7 +53,8 @@ public class TopMenuPanel extends JPanel {
 		gbc_specialMoveButton1.gridy = 1;
 		add(specialMoveButton1, gbc_specialMoveButton1);
 		
-		JButton specialMoveButton2 = new JButton("SpecialMoveButton2");
+		// Special move button 2
+		specialMoveButton2 = new JButton(MOVE_TEXT + 2);
 		specialMoveButton2.setPreferredSize(new Dimension(100, 100));
 		GridBagConstraints gbc_specialMoveButton2 = new GridBagConstraints();
 		gbc_specialMoveButton2.insets = new Insets(0, 0, 5, 5);
@@ -48,7 +62,8 @@ public class TopMenuPanel extends JPanel {
 		gbc_specialMoveButton2.gridy = 1;
 		add(specialMoveButton2, gbc_specialMoveButton2);
 		
-		JButton specialMoveButton3 = new JButton("SpecialMoveButton3");
+		// Special move button 3
+		specialMoveButton3 = new JButton(MOVE_TEXT + 3);
 		specialMoveButton3.setPreferredSize(new Dimension(100, 100));
 		GridBagConstraints gbc_specialMoveButton3 = new GridBagConstraints();
 		gbc_specialMoveButton3.insets = new Insets(0, 0, 5, 5);
@@ -56,14 +71,53 @@ public class TopMenuPanel extends JPanel {
 		gbc_specialMoveButton3.gridy = 1;
 		add(specialMoveButton3, gbc_specialMoveButton3);
 		
-		JButton specialMoveButton4 = new JButton("SpecialMoveButton4");
+		// Special move button 4
+		specialMoveButton4 = new JButton(MOVE_TEXT + 4);
 		specialMoveButton4.setPreferredSize(new Dimension(100, 100));
 		GridBagConstraints gbc_specialMoveButton4 = new GridBagConstraints();
 		gbc_specialMoveButton4.insets = new Insets(0, 0, 5, 5);
 		gbc_specialMoveButton4.gridx = 9;
 		gbc_specialMoveButton4.gridy = 1;
 		add(specialMoveButton4, gbc_specialMoveButton4);
-
 	}
-
+	
+	/**
+	 * @brief Returns the level name text field.
+	 * @return this.levelNameTextField The level name text field.
+	 */
+	public JTextField getLevelNameTextField() {
+		return this.levelNameTextField;
+	}
+	
+	/**
+	 * @brief Returns the special move button 1.
+	 * @return this.specialMoveButton1 The special move button 1.
+	 */
+	public JButton getSpecialMoveButton1() {
+		return this.specialMoveButton1;
+	}
+	
+	/**
+	 * @brief Returns the special move button 2.
+	 * @return this.specialMoveButton1 The special move button 2.
+	 */
+	public JButton getSpecialMoveButton2() {
+		return this.specialMoveButton2;
+	}
+	
+	/**
+	 * @brief Returns the special move button 3.
+	 * @return this.specialMoveButton1 The special move button 3.
+	 */
+	public JButton getSpecialMoveButton3() {
+		return this.specialMoveButton3;
+	}
+	
+	/**
+	 * @brief Returns the special move button 4.
+	 * @return this.specialMoveButton1 The special move button 4.
+	 */
+	public JButton getSpecialMoveButton4() {
+		return this.specialMoveButton4;
+	}
 }
