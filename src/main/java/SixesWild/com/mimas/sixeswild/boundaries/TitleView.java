7bce41c4e949ef64e6acb5861cae058bcd8d5349
Cameron Jones <ctjones@wpi.edu>
2015-04-13 12:23:16 -0400

    Added TitleView - Under Development
    Started Developing TitleView

---
+++ src/main/java/SixesWild/com/mimas/sixeswild/boundaries/TitleView.java
@@ -0,0 +1,14 @@
+package SixesWild.com.mimas.sixeswild.boundaries;
+
+import javax.swing.JPanel;
+
+public class TitleView extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public TitleView() {
+
+	}
+
+}
