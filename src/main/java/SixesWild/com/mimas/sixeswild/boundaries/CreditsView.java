144d40eb965d6b6b550a1a8c4d2cfcc2314a7110
Joey Perez <jwperezwpi@gmail.com>
2015-04-13 17:40:11 -0400

    Updated the CreditsView to included colorful text and a better background color.

--- src/main/java/SixesWild/com/mimas/sixeswild/boundaries/CreditsView.java
+++ src/main/java/SixesWild/com/mimas/sixeswild/boundaries/CreditsView.java
@@ -15,6 +15,8 @@ import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.SystemColor;
 /**
  * @author Joey Perez
  *
@@ -29,7 +31,7 @@ public class CreditsView extends JPanel {
  */
 	public CreditsView(){
 		setForeground(Color.BLUE);
-		setBackground(Color.LIGHT_GRAY);
+		setBackground(SystemColor.menu);
 		GridBagLayout gridBagLayout = new GridBagLayout();
 		gridBagLayout.columnWidths = new int[]{187, 0};
 		gridBagLayout.rowHeights = new int[]{47, 0, 0, 0, 0, 0, 14, 0, 0, 0, 0, 0, 0};
@@ -38,6 +40,7 @@ public class CreditsView extends JPanel {
 		setLayout(gridBagLayout);
 		
 		JLabel lblNewLabel = new JLabel("=========CREDITS=========");
+		lblNewLabel.setFont(new Font("Comic Sans MS", Font.BOLD, 18));
 		lblNewLabel.setVerticalAlignment(SwingConstants.TOP);
 		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
 		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 0);
@@ -46,6 +49,8 @@ public class CreditsView extends JPanel {
 		add(lblNewLabel, gbc_lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Joey Perez\r\n");
+		lblNewLabel_1.setFont(new Font("Comic Sans MS", Font.BOLD, 16));
+		lblNewLabel_1.setForeground(Color.BLUE);
 		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
 		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 0);
 		gbc_lblNewLabel_1.gridx = 0;
@@ -53,6 +58,8 @@ public class CreditsView extends JPanel {
 		add(lblNewLabel_1, gbc_lblNewLabel_1);
 		
 		JLabel lblNewLabel_2 = new JLabel("Marco Duran");
+		lblNewLabel_2.setForeground(Color.MAGENTA);
+		lblNewLabel_2.setFont(new Font("Comic Sans MS", Font.BOLD, 16));
 		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
 		gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 0);
 		gbc_lblNewLabel_2.gridx = 0;
@@ -60,6 +67,8 @@ public class CreditsView extends JPanel {
 		add(lblNewLabel_2, gbc_lblNewLabel_2);
 		
 		JLabel lblNewLabel_3 = new JLabel("Cameron Jones");
+		lblNewLabel_3.setFont(new Font("Comic Sans MS", Font.BOLD, 16));
+		lblNewLabel_3.setForeground(new Color(30, 144, 255));
 		GridBagConstraints gbc_lblNewLabel_3 = new GridBagConstraints();
 		gbc_lblNewLabel_3.insets = new Insets(0, 0, 5, 0);
 		gbc_lblNewLabel_3.gridx = 0;
@@ -67,6 +76,8 @@ public class CreditsView extends JPanel {
 		add(lblNewLabel_3, gbc_lblNewLabel_3);
 		
 		JLabel lblNewLabel_4 = new JLabel("Aditya Nivarthi");
+		lblNewLabel_4.setFont(new Font("Comic Sans MS", Font.BOLD, 16));
+		lblNewLabel_4.setForeground(Color.BLUE);
 		GridBagConstraints gbc_lblNewLabel_4 = new GridBagConstraints();
 		gbc_lblNewLabel_4.insets = new Insets(0, 0, 5, 0);
 		gbc_lblNewLabel_4.gridx = 0;
@@ -74,6 +85,8 @@ public class CreditsView extends JPanel {
 		add(lblNewLabel_4, gbc_lblNewLabel_4);
 		
 		JLabel lblNewLabel_5 = new JLabel("Yahel Nachum");
+		lblNewLabel_5.setForeground(Color.MAGENTA);
+		lblNewLabel_5.setFont(new Font("Comic Sans MS", Font.BOLD, 16));
 		GridBagConstraints gbc_lblNewLabel_5 = new GridBagConstraints();
 		gbc_lblNewLabel_5.insets = new Insets(0, 0, 5, 0);
 		gbc_lblNewLabel_5.gridx = 0;
