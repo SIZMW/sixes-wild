a5f57ac294758374eb2c6c5cadd29a6e5ca16939
Cameron Jones <ctjones@wpi.edu>
2015-04-13 17:01:32 -0400

    Removed Unneed Imports

--- src/main/java/SixesWild/com/mimas/sixeswild/boundaries/GameApplication.java
+++ src/main/java/SixesWild/com/mimas/sixeswild/boundaries/GameApplication.java
@@ -1,8 +1,6 @@
 package SixesWild.com.mimas.sixeswild.boundaries;
 
 import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.UIManager;
