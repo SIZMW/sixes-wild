4b91ca5cfeb599b3b9f4008537949b38dbf8f123
Joey Perez <jwperezwpi@gmail.com>
2015-04-15 13:56:57 -0400

    Added two tests to TestLevel
    Started to test appearance and multiplier frequency

--- src/test/java/SixesWild/com/mimas/sixeswild/entities/TestLevel.java
+++ src/test/java/SixesWild/com/mimas/sixeswild/entities/TestLevel.java
@@ -1,9 +1,29 @@
 package SixesWild.com.mimas.sixeswild.entities;
+import java.util.ArrayList;
+
 import junit.framework.TestCase;
 
 public class TestLevel extends TestCase {
 	
-	public TestLevel() {
+	public void testLevel() {
+		Level level = new Level();
+		ArrayList<Double> freqs = new ArrayList<Double>();
+		ArrayList<Double> mFreqs = new ArrayList<Double>();
+		freqs.add(.10);
+		freqs.add(.20);
+		freqs.add(.30);
+		freqs.add(.30);
+		freqs.add(.05);
+		freqs.add(.05);
+		
+		mFreqs.add(.95);
+		mFreqs.add(.025);
+		mFreqs.add(.025);
+		
+		
+		assertEquals(level.getTileFreq(), freqs);
+		assertEquals(level.getMultFreq(), mFreqs);
+		
 		
 	}
 
