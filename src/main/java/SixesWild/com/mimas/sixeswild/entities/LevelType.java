3f62b38890b46d6852ce9cfed53f5be30a578bd5
Cameron Jones <ctjones@wpi.edu>
2015-04-15 13:37:54 -0400

    Developing Level Class (WIP) and TestLevel test
    started the Level Class and corresponding unit test.

---
+++ src/main/java/SixesWild/com/mimas/sixeswild/entities/LevelType.java
@@ -0,0 +1,5 @@
+package SixesWild.com.mimas.sixeswild.entities;
+
+public enum LevelType {
+	PUZZLE, RELEASE, LIGHTNING, ELIMINATION
+}
---
+++ src/test/java/SixesWild/com/mimas/sixeswild/entities/TestLevel.java
@@ -0,0 +1,10 @@
+package SixesWild.com.mimas.sixeswild.entities;
+import junit.framework.TestCase;
+
+public class TestLevel extends TestCase {
+	
+	public TestLevel() {
+		
+	}
+
+}
