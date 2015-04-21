5822fe19a671dc58e91de2172e8c210070e8d2e5
Cameron Jones <ctjones@wpi.edu>
2015-04-21 12:47:18 -0400

    Revert "Added Proper Documentation"
    This reverts commit a1dbed69051167783b6cb5eac5923ad8321d7e8f.

--- src/main/java/SixesWild/com/mimas/sixeswild/entities/PuzzleLevel.java
+++ src/main/java/SixesWild/com/mimas/sixeswild/entities/PuzzleLevel.java
@@ -2,10 +2,6 @@ package SixesWild.com.mimas.sixeswild.entities;
 
 import java.util.ArrayList;
 
-/**
- * @author Cameron
- *
- */
 public class PuzzleLevel extends Level {
 	LevelType type;
 	ArrayList<Double> tileFreq;
@@ -17,15 +13,6 @@ public class PuzzleLevel extends Level {
 	int moveCount;
 	boolean isUnlocked;
 	
-	/**
-	 * @param tileFreq
-	 * @param multFreq
-	 * @param name
-	 * @param squares
-	 * @param pointThresholds
-	 * @param moveCount
-	 * @param isUnlocked
-	 */
 	public PuzzleLevel(ArrayList<Double> tileFreq, ArrayList<Double> multFreq,
 		String name, ArrayList<Square> squares,
 		PointThresholds pointThresholds, int moveCount, boolean isUnlocked) {
@@ -45,9 +32,6 @@ public class PuzzleLevel extends Level {
 		this.isUnlocked = isUnlocked;
 	}
 	
-	/**
-	 * 
-	 */
 	public PuzzleLevel() {
 		this.tileFreq = new ArrayList<Double>();
 		this.multFreq = new ArrayList<Double>();
@@ -68,9 +52,6 @@ public class PuzzleLevel extends Level {
 		this.isUnlocked = true;
 	}
 	
-	/**
-	 * @param squares
-	 */
 	public PuzzleLevel(ArrayList<Square> squares) {
 		this.tileFreq = new ArrayList<Double>();
 		this.tileFreq.add(.10);
@@ -90,124 +71,77 @@ public class PuzzleLevel extends Level {
 		this.isUnlocked = true;
 	}
 
-	/* (non-Javadoc)
-	 * @see SixesWild.com.mimas.sixeswild.entities.Level#getType()
-	 */
 	public LevelType getType() {
 		return type;
 	}
 
-	/* (non-Javadoc)
-	 * @see SixesWild.com.mimas.sixeswild.entities.Level#getTileFreq()
-	 */
+	public void setType(LevelType type) {
+		this.type = type;
+	}
+
 	public ArrayList<Double> getTileFreq() {
 		return tileFreq;
 	}
 
-	/* (non-Javadoc)
-	 * @see SixesWild.com.mimas.sixeswild.entities.Level#setTileFreq(java.util.ArrayList)
-	 */
 	public void setTileFreq(ArrayList<Double> tileFreq) {
 		this.tileFreq = tileFreq;
 	}
 
-	/* (non-Javadoc)
-	 * @see SixesWild.com.mimas.sixeswild.entities.Level#getMultFreq()
-	 */
 	public ArrayList<Double> getMultFreq() {
 		return multFreq;
 	}
 
-	/* (non-Javadoc)
-	 * @see SixesWild.com.mimas.sixeswild.entities.Level#setMultFreq(java.util.ArrayList)
-	 */
 	public void setMultFreq(ArrayList<Double> multFreq) {
 		this.multFreq = multFreq;
 	}
 
-	/**
-	 * @return
-	 */
 	public SpecialMoves getSpecialMoves() {
 		return specialMoves;
 	}
 
-	/**
-	 * @param specialMoves
-	 */
 	public void setSpecialMoves(SpecialMoves specialMoves) {
 		this.specialMoves = specialMoves;
 	}
 
-	/* (non-Javadoc)
-	 * @see SixesWild.com.mimas.sixeswild.entities.Level#getPointThresholds()
-	 */
 	public PointThresholds getPointThresholds() {
 		return pointThresholds;
 	}
 
-	/* (non-Javadoc)
-	 * @see SixesWild.com.mimas.sixeswild.entities.Level#setPointThresholds(SixesWild.com.mimas.sixeswild.entities.PointThresholds)
-	 */
 	public void setPointThresholds(PointThresholds pointThresholds) {
 		this.pointThresholds = pointThresholds;
 	}
 
-	/* (non-Javadoc)
-	 * @see SixesWild.com.mimas.sixeswild.entities.Level#getName()
-	 */
 	public String getName() {
 		return name;
 	}
 
-	/* (non-Javadoc)
-	 * @see SixesWild.com.mimas.sixeswild.entities.Level#setName(java.lang.String)
-	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
-	/* (non-Javadoc)
-	 * @see SixesWild.com.mimas.sixeswild.entities.Level#getBoard()
-	 */
 	public Board getBoard() {
 		return board;
 	}
 
-	/* (non-Javadoc)
-	 * @see SixesWild.com.mimas.sixeswild.entities.Level#setBoard(SixesWild.com.mimas.sixeswild.entities.Board)
-	 */
 	public void setBoard(Board board) {
 		this.board = board;
 	}
 
-	/* (non-Javadoc)
-	 * @see SixesWild.com.mimas.sixeswild.entities.Level#getMoveCount()
-	 */
 	public int getMoveCount() {
 		return moveCount;
 	}
 
-	/* (non-Javadoc)
-	 * @see SixesWild.com.mimas.sixeswild.entities.Level#setMoveCount(int)
-	 */
 	public void setMoveCount(int moveCount) {
 		this.moveCount = moveCount;
 	}
 
-	/* (non-Javadoc)
-	 * @see SixesWild.com.mimas.sixeswild.entities.Level#isUnlocked()
-	 */
 	public boolean isUnlocked() {
 		return isUnlocked;
 	}
 
-	/* (non-Javadoc)
-	 * @see SixesWild.com.mimas.sixeswild.entities.Level#setUnlocked(boolean)
-	 */
 	public void setUnlocked(boolean isUnlocked) {
 		this.isUnlocked = isUnlocked;
 	}
-}
 
+}
 
