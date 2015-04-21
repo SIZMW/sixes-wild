b533c5595bb6243f551d4b6ae63bb264c4c69eb3
mdduran <mdduran@outlook.com>
2015-04-21 11:43:13 -0400

    Added Badge entity and finished aesthetic test
    I also finished the badge test

---
+++ src/test/java/SixesWild/com/mimas/sixeswild/entities/TestAesthetic.java
@@ -0,0 +1,46 @@
+package SixesWild.com.mimas.sixeswild.entities;
+
+import java.awt.Color;
+
+import junit.framework.TestCase;
+/**
+ * This test case handles the aesthetic options.
+ * @author Marco
+ *
+ */
+public class TestAesthetic extends TestCase {
+	Aesthetic aesthetic;
+	Color red;
+	Color black;
+	Color blue;
+	Color green;
+	Color magenta;
+	Color yellow;
+	Color gray;
+	public void setUp(){
+		red = Color.RED;
+		black = Color.BLACK;
+		blue = Color.BLUE;
+		green = Color.GREEN;
+		magenta = Color.MAGENTA;
+		yellow = Color.YELLOW;
+		gray = Color.GRAY;
+		
+		aesthetic = new Aesthetic(gray, red, black, blue, green, magenta, yellow);
+	
+	}
+	
+	public void tearDown(){}
+	
+	/** Tests all of the Aesthetic methods */
+	public void testAestheticMethods(){
+		assertEquals(aesthetic.getBackgroundColor(), gray);
+		assertEquals(aesthetic.getTileOneColor(), red);
+		assertEquals(aesthetic.getTileTwoColor(), black);
+		assertEquals(aesthetic.getTileThreeColor(), blue);
+		assertEquals(aesthetic.getTileFourColor(), green);
+		assertEquals(aesthetic.getTileFiveColor(), magenta);
+		assertEquals(aesthetic.getTileSixColor(), yellow);
+		
+	}
+}
---
+++ src/test/java/SixesWild/com/mimas/sixeswild/entities/TestBadge.java
@@ -0,0 +1,45 @@
+package SixesWild.com.mimas.sixeswild.entities;
+
+import junit.framework.TestCase;
+/**
+ * This test handles the all aspects relating to the badge.
+ * @author Marco Duran
+ *
+ */
+public class TestBadge extends TestCase {
+	Badge badge;
+	String name;
+	String description;
+	boolean isUnlocked;
+	/**
+	 * Created a badge with filled parameters
+	 */
+	public void setUp(){
+		String name = "Test1";
+		String description = "This is a test";
+		boolean isUnlocked = false;
+		
+		badge = new Badge(name, description, isUnlocked);
+	}
+	
+	public void tearDown(){}
+	
+	public void testBadgeMethods(){
+		assertFalse(badge.unlockNow());
+		assertFalse(badge.isUnlocked);
+		
+		assertEquals(badge.getName(), badge.name);
+		assertEquals(badge.getDescription(), badge.description);
+		
+		badge.unlock();
+		assertTrue(badge.unlockNow());
+		assertTrue(badge.isUnlocked);
+		
+		badge.lock();
+		assertFalse(badge.unlockNow());
+		assertFalse(badge.isUnlocked);
+		
+	}
+	
+	
+}
