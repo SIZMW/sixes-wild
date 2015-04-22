5a9cd495a688b3b531a3179926f79192f6685719
Aditya <adityanivarthi19@gmail.com>
2015-04-22 13:24:45 -0400

    Formatted test cases.

--- src/test/java/SixesWild/com/mimas/sixeswild/entities/TestPuzzleLevel.java
+++ src/test/java/SixesWild/com/mimas/sixeswild/entities/TestPuzzleLevel.java
@@ -15,13 +15,13 @@ public class TestPuzzleLevel extends TestCase {
 	 * Test all methods for Level.
 	 */
 	public void testPuzzleLevel() {
-		ArrayList<Double> tileFreq = new ArrayList<Double>();    //DONE
-		ArrayList<Double> multFreq = new ArrayList<Double>();    //DONE
-		LevelType levelType = LevelType.PUZZLE;                  //DONE
-		String levelName = "testPuzzle";                         //DONE
+		ArrayList<Double> tileFreq = new ArrayList<Double>(); // DONE
+		ArrayList<Double> multFreq = new ArrayList<Double>(); // DONE
+		LevelType levelType = LevelType.PUZZLE; // DONE
+		String levelName = "testPuzzle"; // DONE
 		Tile tiles[][] = new Tile[9][9];
-		PointThresholds pointThresholds = new PointThresholds(); //DONE
-		
+		PointThresholds pointThresholds = new PointThresholds(); // DONE
+
 		tileFreq.add(.10);
 		tileFreq.add(.20);
 		tileFreq.add(.30);
@@ -32,42 +32,42 @@ public class TestPuzzleLevel extends TestCase {
 		multFreq.add(.95);
 		multFreq.add(.025);
 		multFreq.add(.025);
-		
-		for(int i = 0; i < 9; i++){
-			for(int j = 0; j < 9; j++){
+
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
 				tiles[i][j] = new NumberTile(3, 2);
 			}
 		}
-		
+
 		PuzzleLevel level;
 		try {
-			level = new PuzzleLevel(tileFreq, multFreq, levelType, levelName, tiles, pointThresholds, 50);
+			level = new PuzzleLevel(tileFreq, multFreq, levelType, levelName,
+					tiles, pointThresholds, 50);
 			assertEquals(levelName, level.name);
 			assertEquals(LevelType.PUZZLE, level.type);
 			assertEquals(tileFreq, level.board.tileFrequencies);
 			assertEquals(pointThresholds, level.pointThresholds);
 			assertEquals(multFreq, level.board.multiplierFrequencies);
-			//assertEquals(tiles)
 		} catch (Exception e1) {
 			e1.printStackTrace();
 			fail();
 		}
-		
-		
-		
+
 		tiles[0][0] = null;
 		try {
-		     PuzzleLevel failure = new PuzzleLevel(tileFreq, multFreq, levelType, levelName, tiles, pointThresholds, 9);
-		     fail( "Missing Exception" );
-		} catch( Exception e ) {
-		     assertTrue(true);
+			new PuzzleLevel(tileFreq, multFreq, levelType, levelName, tiles,
+					pointThresholds, 9);
+			fail("Missing Exception");
+		} catch (Exception e) {
+			assertTrue(true);
 		}
-		
+
 		try {
-		     PuzzleLevel failure = new PuzzleLevel(tileFreq, multFreq, levelType, levelName, null, pointThresholds, 9);
-		     fail( "Missing Exception" );
-		} catch( Exception e ) {
-		     assertTrue(true);
+			new PuzzleLevel(tileFreq, multFreq, levelType, levelName, null,
+					pointThresholds, 9);
+			fail("Missing Exception");
+		} catch (Exception e) {
+			assertTrue(true);
 		}
 	}
 }
--- src/test/java/SixesWild/com/mimas/sixeswild/entities/TestReleaseLevel.java
+++ src/test/java/SixesWild/com/mimas/sixeswild/entities/TestReleaseLevel.java
@@ -15,13 +15,13 @@ public class TestReleaseLevel extends TestCase {
 	 * Test all methods for ReleaseLevel.
 	 */
 	public void testPuzzleLevel() {
-		ArrayList<Double> tileFreq = new ArrayList<Double>();    //DONE
-		ArrayList<Double> multFreq = new ArrayList<Double>();    //DONE
-		LevelType levelType = LevelType.RELEASE;                 //DONE
-		String levelName = "testRelease";                        //DONE
+		ArrayList<Double> tileFreq = new ArrayList<Double>(); // DONE
+		ArrayList<Double> multFreq = new ArrayList<Double>(); // DONE
+		LevelType levelType = LevelType.RELEASE; // DONE
+		String levelName = "testRelease"; // DONE
 		Tile tiles[][] = new Tile[9][9];
-		PointThresholds pointThresholds = new PointThresholds(); //DONE
-		
+		PointThresholds pointThresholds = new PointThresholds(); // DONE
+
 		tileFreq.add(.10);
 		tileFreq.add(.20);
 		tileFreq.add(.30);
@@ -32,42 +32,42 @@ public class TestReleaseLevel extends TestCase {
 		multFreq.add(.95);
 		multFreq.add(.025);
 		multFreq.add(.025);
-		
-		for(int i = 0; i < 9; i++){
-			for(int j = 0; j < 9; j++){
+
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
 				tiles[i][j] = new NumberTile(3, 2);
 			}
 		}
-		
+
 		ReleaseLevel level;
 		try {
-			level = new ReleaseLevel(tileFreq, multFreq, levelType, levelName, tiles, pointThresholds, 50);
+			level = new ReleaseLevel(tileFreq, multFreq, levelType, levelName,
+					tiles, pointThresholds, 50);
 			assertEquals(levelName, level.name);
 			assertEquals(LevelType.RELEASE, level.type);
 			assertEquals(tileFreq, level.board.tileFrequencies);
 			assertEquals(pointThresholds, level.pointThresholds);
 			assertEquals(multFreq, level.board.multiplierFrequencies);
-			//assertEquals(tiles)
 		} catch (Exception e1) {
 			e1.printStackTrace();
 			fail();
 		}
-		
-		
-		
+
 		tiles[0][0] = null;
 		try {
-		     ReleaseLevel failure = new ReleaseLevel(tileFreq, multFreq, levelType, levelName, tiles, pointThresholds, 9);
-		     fail( "Missing Exception" );
-		} catch( Exception e ) {
-		     assertTrue(true);
+			new ReleaseLevel(tileFreq, multFreq, levelType, levelName, tiles,
+					pointThresholds, 9);
+			fail("Missing Exception");
+		} catch (Exception e) {
+			assertTrue(true);
 		}
-		
+
 		try {
-			ReleaseLevel failure = new ReleaseLevel(tileFreq, multFreq, levelType, levelName, null, pointThresholds, 9);
-		     fail( "Missing Exception" );
-		} catch( Exception e ) {
-		     assertTrue(true);
+			new ReleaseLevel(tileFreq, multFreq, levelType, levelName, null,
+					pointThresholds, 9);
+			fail("Missing Exception");
+		} catch (Exception e) {
+			assertTrue(true);
 		}
 	}
 }
