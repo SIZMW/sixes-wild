552282450a7be94f4af3ed7680ee24cf9a9eb55f
Aditya <adityanivarthi19@gmail.com>
2015-04-23 12:30:58 -0400

    Added test case for menu types enumerated type.

---
+++ src/test/java/SixesWild/com/mimas/sixeswild/entities/TestMenuTypesEnum.java
@@ -0,0 +1,21 @@
+package SixesWild.com.mimas.sixeswild.entities;
+
+import junit.framework.TestCase;
+
+/**
+ * This test case handles testing the MenuTypes enumerated type.
+ * 
+ * @author Aditya Nivarthi
+ */
+public class TestMenuTypesEnum extends TestCase {
+
+	/**
+	 * Tests the verify the type values in the MenuTypes class.
+	 */
+	public void testTileTypeEnum() {
+		MenuTypes types[] = MenuTypes.values();
+		assertEquals(types.length, 2);
+		assertEquals(types[0], MenuTypes.STORY);
+		assertEquals(types[1], MenuTypes.USER);
+	}
+}
