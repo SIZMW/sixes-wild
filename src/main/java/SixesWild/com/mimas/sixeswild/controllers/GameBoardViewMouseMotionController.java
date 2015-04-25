d25969be7dcb3d0de5dde8f03b1fe739421995d6
Cameron Jones <ctjones@wpi.edu>
2015-04-25 17:04:09 -0400

    Level Now Exits when Out of Moves via a PopUp Dialouge that explains the reason

--- src/main/java/SixesWild/com/mimas/sixeswild/controllers/GameBoardViewMouseMotionController.java
+++ src/main/java/SixesWild/com/mimas/sixeswild/controllers/GameBoardViewMouseMotionController.java
@@ -5,12 +5,18 @@ import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
 import SixesWild.com.mimas.sixeswild.boundaries.GameApplication;
 import SixesWild.com.mimas.sixeswild.entities.SelectionMove;
@@ -55,35 +61,25 @@ public class GameBoardViewMouseMotionController extends MouseAdapter {
 		app.getLevelPanel().updateLevelStats();
 		
 		if(this.app.getLevelPanel().getLevel().getMoveCount() <= 0){
-			//This is a direct Copy from the Exit Button. We should Find a Better Way
-			Container contentContainer = app.getFrame().getContentPane();
-			JPanel currentPanel = new JPanel();
-			contentContainer.removeAll();
-
-			// Layout for panel
-			GridBagLayout gridBagLayout = new GridBagLayout();
-			gridBagLayout.columnWidths = new int[] { 0 };
-			gridBagLayout.rowHeights = new int[] { 0 };
-			gridBagLayout.columnWeights = new double[] { 1.0, Double.MIN_VALUE };
-			gridBagLayout.rowWeights = new double[] { 1.0, Double.MIN_VALUE };
-			currentPanel.setPreferredSize(new Dimension(800, 600));
-			currentPanel.setLayout(gridBagLayout);
-
-			GridBagConstraints gbc_list = new GridBagConstraints();
-			gbc_list.insets = new Insets(0, 0, 0, 0);
-			gbc_list.fill = GridBagConstraints.BOTH;
-			gbc_list.gridx = 0;
-			gbc_list.gridy = 0;
-
-			// Add panel to view
-			currentPanel.add(this.app.getGameMenuView(), gbc_list);
-			contentContainer.add(currentPanel);
-			contentContainer.revalidate();
-			contentContainer.repaint();
-
-			logger.log(Level.INFO, "Exited level.");
+			final JOptionPane optionPane = new JOptionPane();
+		    optionPane.setMessage("Out of Moves");
+		    optionPane.setMessageType(JOptionPane.INFORMATION_MESSAGE);
+		    final JButton okButton = new JButton("OK");
+			okButton.addActionListener(new ExitButtonController(this.app));
+			ActionListener actionListener = new ActionListener() {
+			      public void actionPerformed(ActionEvent actionEvent) {
+			        // Return current text label, instead of argument to method
+			    	  SwingUtilities.getWindowAncestor(okButton).dispose();
+			      }
+			    };
+			    okButton.addActionListener(actionListener);
+		    optionPane.setOptions(new Object[] { okButton });
+		    JDialog dialog = optionPane.createDialog(this.app.getFrame(), "");
+		    dialog.setVisible(true);
+		}
+		else{
+			app.getLevelPanel().getBoardViewPanel().updateUI();
 		}
 		
-		app.getLevelPanel().getBoardViewPanel().updateUI();
 	}
 }
