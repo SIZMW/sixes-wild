0382353f46ddf7161246d294df5e5fd61ec1478f
Cameron Jones <ctjones@wpi.edu>
2015-04-25 16:29:57 -0400

    Level Exits when there are No more moves
    This is an intermediate stage, I am investigating adding a popup message
    to indicate the level is over

--- src/main/java/SixesWild/com/mimas/sixeswild/controllers/GameBoardViewMouseController.java
+++ src/main/java/SixesWild/com/mimas/sixeswild/controllers/GameBoardViewMouseController.java
@@ -1,7 +1,16 @@
 package SixesWild.com.mimas.sixeswild.controllers;
 
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.swing.JPanel;
 
 import SixesWild.com.mimas.sixeswild.boundaries.GameApplication;
 import SixesWild.com.mimas.sixeswild.entities.SelectionMove;
@@ -14,6 +23,8 @@ import SixesWild.com.mimas.sixeswild.entities.SelectionMove;
  */
 public class GameBoardViewMouseController extends MouseAdapter {
 
+	private static final Logger logger = Logger.getGlobal();
+	
 	protected GameApplication app;
 
 	/**
@@ -57,6 +68,37 @@ public class GameBoardViewMouseController extends MouseAdapter {
 		move.doMove(this.app);
 
 		app.getLevelPanel().updateLevelStats();
+		
+		if(this.app.getLevelPanel().getLevel().getMoveCount() <= 0){
+			//This is a direct Copy from the Exit Button. We should Find a Better Way
+			Container contentContainer = app.getFrame().getContentPane();
+			JPanel currentPanel = new JPanel();
+			contentContainer.removeAll();
+
+			// Layout for panel
+			GridBagLayout gridBagLayout = new GridBagLayout();
+			gridBagLayout.columnWidths = new int[] { 0 };
+			gridBagLayout.rowHeights = new int[] { 0 };
+			gridBagLayout.columnWeights = new double[] { 1.0, Double.MIN_VALUE };
+			gridBagLayout.rowWeights = new double[] { 1.0, Double.MIN_VALUE };
+			currentPanel.setPreferredSize(new Dimension(800, 600));
+			currentPanel.setLayout(gridBagLayout);
+
+			GridBagConstraints gbc_list = new GridBagConstraints();
+			gbc_list.insets = new Insets(0, 0, 0, 0);
+			gbc_list.fill = GridBagConstraints.BOTH;
+			gbc_list.gridx = 0;
+			gbc_list.gridy = 0;
+
+			// Add panel to view
+			currentPanel.add(this.app.getGameMenuView(), gbc_list);
+			contentContainer.add(currentPanel);
+			contentContainer.revalidate();
+			contentContainer.repaint();
+
+			logger.log(Level.INFO, "Exited level.");
+		}
+		
 		app.getLevelPanel().getBoardViewPanel().updateUI();
 	}
 }
