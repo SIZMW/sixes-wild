c2f46d80155d5414e83bc61089c4bcfc21e95f10
Cameron Jones <ctjones@wpi.edu>
2015-04-27 16:59:01 -0400

    Added an Optional TestCase Framework to add easier mouseactions not sure how great it is

---
+++ src/test/java/SixesWild/com/mimas/sixeswild/testcase/SixesWildTestCase.java
@@ -0,0 +1,73 @@
+package SixesWild.com.mimas.sixeswild.testcase;
+
+import java.awt.event.MouseEvent;
+
+import SixesWild.com.mimas.sixeswild.boundaries.BuilderApplication;
+import SixesWild.com.mimas.sixeswild.boundaries.GameApplication;
+import junit.framework.TestCase;
+
+public class SixesWildTestCase extends TestCase{
+	/** (dx,dy) are coordinates. Feel Free to Use as Is. */
+	public MouseEvent createPressed (BuilderApplication game, int dx, int dy) {
+		MouseEvent me = new MouseEvent(game.getFrame(), MouseEvent.MOUSE_PRESSED, 
+				System.currentTimeMillis(), 0, 
+				dx, dy, 0, false);
+		return me;
+	}
+		
+	/** (dx,dy) are coordinates. Feel Free to Use as Is. */
+	public MouseEvent createReleased (BuilderApplication game, int dx, int dy) {
+		MouseEvent me = new MouseEvent(game.getFrame(), MouseEvent.MOUSE_RELEASED, 
+				System.currentTimeMillis(), 0, 
+				dx, dy, 0, false);
+		return me;
+	}
+	
+	/** (dx,dy) are coordinates. Feel Free to Use as Is. */
+	public MouseEvent createClicked (BuilderApplication game, int dx, int dy) {
+		MouseEvent me = new MouseEvent(game.getFrame(), MouseEvent.MOUSE_CLICKED, 
+				System.currentTimeMillis(), 0, 
+				dx, dy, 1, false);
+		return me;
+	}
+	
+	/** (dx,dy) are coordinates. Feel Free to Use as Is. */
+	public MouseEvent createDoubleClicked (BuilderApplication game, int dx, int dy) {
+		MouseEvent me = new MouseEvent(game.getFrame(), MouseEvent.MOUSE_CLICKED, 
+				System.currentTimeMillis(), 0, 
+				dx, dy, 2, false);
+		return me;
+	}
+	
+	/** (dx,dy) are coordinates. Feel Free to Use as Is. */
+	public MouseEvent createPressed (GameApplication game, int dx, int dy) {
+		MouseEvent me = new MouseEvent(game.getFrame(), MouseEvent.MOUSE_PRESSED, 
+				System.currentTimeMillis(), 0, 
+				dx, dy, 0, false);
+		return me;
+	}
+		
+	/** (dx,dy) are coordinates. Feel Free to Use as Is. */
+	public MouseEvent createReleased (GameApplication game, int dx, int dy) {
+		MouseEvent me = new MouseEvent(game.getFrame(), MouseEvent.MOUSE_RELEASED, 
+				System.currentTimeMillis(), 0, 
+				dx, dy, 0, false);
+		return me;
+	}
+	
+	/** (dx,dy) are coordinates. Feel Free to Use as Is. */
+	public MouseEvent createClicked (GameApplication game, int dx, int dy) {
+		MouseEvent me = new MouseEvent(game.getFrame(), MouseEvent.MOUSE_CLICKED, 
+				System.currentTimeMillis(), 0, 
+				dx, dy, 1, false);
+		return me;
+	}
+	
+	/** (dx,dy) are coordinates. Feel Free to Use as Is. */
+	public MouseEvent createDoubleClicked (GameApplication game, int dx, int dy) {
+		MouseEvent me = new MouseEvent(game.getFrame(), MouseEvent.MOUSE_CLICKED, 
+				System.currentTimeMillis(), 0, 
+				dx, dy, 2, false);
+		return me;
+	}
+}
