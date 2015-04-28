793725ff9d1e6d8a8ad797ae3ba4e93d54df4cd8
mdduran <mdduran@outlook.com>
2015-04-28 17:57:51 -0400

    Added unlock method for ScoreBadge Class.

--- src/main/java/SixesWild/com/mimas/sixeswild/entities/ScoreBadge.java
+++ src/main/java/SixesWild/com/mimas/sixeswild/entities/ScoreBadge.java
@@ -1,10 +1,45 @@
 package SixesWild.com.mimas.sixeswild.entities;
 
+/**
+ * 
+ * @author Marco Duran
+ * 
+ *         Checks to see if the score for the level has reached the criteria of
+ *         the badge.
+ *
+ */
 public class ScoreBadge extends Badge {
+	int criteriaScore;
 
-	public ScoreBadge(String name, String description, boolean isUnlocked) {
+	/**
+	 * Constructor for a score badge.
+	 * 
+	 * @param name
+	 *            Name of badge
+	 * @param description
+	 *            Description of badge
+	 * @param isUnlocked
+	 *            Verify if badge is unlocked
+	 * @param criteriaScore
+	 *            Score to be fulfilled
+	 */
+	public ScoreBadge(String name, String description, boolean isUnlocked,
+			int criteriaScore) {
 		super(name, description, isUnlocked);
-		// TODO Auto-generated constructor stub
+		this.criteriaScore = criteriaScore;
 	}
 
+	/**
+	 * Unlocks the badge.
+	 * 
+	 * @param score
+	 *            Score of the current game, is compared to criteria score.
+	 */
+	public void unlock(int score) {
+		if (score > criteriaScore) {
+			this.isUnlocked = true;
+		} else {
+			this.isUnlocked = false;
+		}
+	}
 }
