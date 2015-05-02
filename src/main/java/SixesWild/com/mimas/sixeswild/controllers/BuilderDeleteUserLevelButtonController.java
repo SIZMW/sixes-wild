472bc92f15f5e1c7ef0cb2f3d2dece904ef27c4b
Aditya <adityanivarthi19@gmail.com>
2015-05-02 17:40:10 -0400

    Modified a user level to have the correct number. Added file renaming and level number updating when a level file is deleted. Test cases are still working. Coverage is down to 91.7%.

--- src/main/java/SixesWild/com/mimas/sixeswild/controllers/BuilderDeleteUserLevelButtonController.java
+++ src/main/java/SixesWild/com/mimas/sixeswild/controllers/BuilderDeleteUserLevelButtonController.java
@@ -3,10 +3,12 @@ package SixesWild.com.mimas.sixeswild.controllers;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import SixesWild.com.mimas.sixeswild.boundaries.BuilderApplication;
+import SixesWild.com.mimas.sixeswild.entities.GameLevel;
 import SixesWild.com.mimas.sixeswild.util.XMLParser;
 
 /**
@@ -33,7 +35,7 @@ public class BuilderDeleteUserLevelButtonController implements ActionListener {
 
 	/*
 	 * (non-Javadoc)
-	 *
+	 * 
 	 * @see
 	 * java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
 	 */
@@ -46,6 +48,26 @@ public class BuilderDeleteUserLevelButtonController implements ActionListener {
 					.getDeleteComboBox().getSelectedIndex()
 					+ 1 + ".xml";
 
+			// Get all level files to rename
+			ArrayList<String> fileNamesToUpdate = new ArrayList<String>();
+			ArrayList<GameLevel> levelsToUpdate = new ArrayList<GameLevel>();
+
+			// Add the level files that need the number to be updated and modify
+			// the level object number
+			for (int i = app.getBuilderView().getBuilderTopPanel()
+					.getDeleteComboBox().getSelectedIndex() + 1, j = 0; i < app
+					.getBuilderView().getBuilderTopPanel().getDeleteComboBox()
+					.getItemCount(); i++, j++) {
+				fileNamesToUpdate.add(i + 1 + ".xml");
+				levelsToUpdate.add(XMLParser.fileToLevel(XMLParser.USER_DIR
+						+ fileNamesToUpdate.get(j)));
+				levelsToUpdate.get(j).setLevelNumber(
+						levelsToUpdate.get(j).getLevelNumber() - 1);
+			}
+
+			logger.log(Level.INFO, "Deleting: " + fileName + ". There are: "
+					+ fileNamesToUpdate.size() + " level files to update.");
+
 			// Open the file to delete
 			try {
 				File level = new File(XMLParser.USER_DIR + fileName);
@@ -57,6 +79,26 @@ public class BuilderDeleteUserLevelButtonController implements ActionListener {
 				}
 			} catch (Exception e) {
 				logger.log(Level.WARNING, "File not found.");
+			}
+
+			// Rename the rest of the level files
+			for (int i = 0; i < levelsToUpdate.size(); i++) {
+
+				// Open the file to delete
+				try {
+					File level = new File(XMLParser.USER_DIR
+							+ fileNamesToUpdate.get(i));
+
+					if (level.delete()) {
+						logger.log(Level.INFO, "Deleted successfully.");
+					} else {
+						logger.log(Level.WARNING, "Delete failed.");
+					}
+				} catch (Exception e) {
+					logger.log(Level.WARNING, "File not found.");
+				}
+
+				XMLParser.levelToFile(levelsToUpdate.get(i));
 			}
 		}
 
