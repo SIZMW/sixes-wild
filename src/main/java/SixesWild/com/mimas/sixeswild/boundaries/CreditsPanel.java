4c7dce3060623d5a469705c48815c5d78d0dbda7
Aditya <adityanivarthi19@gmail.com>
2015-05-05 10:47:32 -0400

    Commented some of the secret controller for credits panel.

--- src/main/java/SixesWild/com/mimas/sixeswild/boundaries/CreditsPanel.java
+++ src/main/java/SixesWild/com/mimas/sixeswild/boundaries/CreditsPanel.java
@@ -5,7 +5,6 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 
-import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
--- src/main/java/SixesWild/com/mimas/sixeswild/controllers/SecretEasterEggController.java
+++ src/main/java/SixesWild/com/mimas/sixeswild/controllers/SecretEasterEggController.java
@@ -1,7 +1,7 @@
 package SixesWild.com.mimas.sixeswild.controllers;
 
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
@@ -9,28 +9,35 @@ import java.nio.file.Paths;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class SecretEasterEggController implements MouseListener {
+public class SecretEasterEggController extends MouseAdapter {
 	private static final Logger logger = Logger.getGlobal();
 	private String person;
-	
+
 	protected Path perez = Paths.get("res\\Bisley.jar");
 	protected Path duran = Paths.get("res\\AmericanToad.jar");
 	protected Path jones = Paths.get("res\\eaglewing.jar");
-	
-	public SecretEasterEggController(String personName){
-		this.person = personName;
-		
+
+	public SecretEasterEggController(String personName) {
+		person = personName;
+
 	}
-	
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.awt.event.MouseAdapter#mouseClicked(java.awt.event.MouseEvent)
+	 */
+	@Override
 	public void mouseClicked(MouseEvent me) {
-		
-		
+
 		Path filePath = Paths.get("");
 		Path creator = getCreator();
 		String s = filePath.toAbsolutePath().toString();
-		logger.log(Level.FINE, "Attempting to open file at location: "  + creator.toAbsolutePath().toString());
-		ProcessBuilder pb = new ProcessBuilder("java", "-jar", creator.toAbsolutePath().toString());
-		logger.log(Level.FINE, "Working in directory: "  + s + "\\res\\");
+		logger.log(Level.FINE, "Attempting to open file at location: "
+				+ creator.toAbsolutePath().toString());
+		ProcessBuilder pb = new ProcessBuilder("java", "-jar", creator
+				.toAbsolutePath().toString());
+		logger.log(Level.FINE, "Working in directory: " + s + "\\res\\");
 		pb.directory(new File(s + "/res/"));
 		try {
 			pb.start();
@@ -39,52 +46,23 @@ public class SecretEasterEggController implements MouseListener {
 			x.printStackTrace();
 		}
 	}
-	
-	private Path getCreator()
-	{
-		if(person == "Marco")
-		{
+
+	private Path getCreator() {
+		if (person == "Marco") {
 			logger.log(Level.FINE, "Mouse clicked on " + person);
 			return duran;
-			
+
 		}
-		if(person == "Joey")
-			{
+		if (person == "Joey") {
 			logger.log(Level.FINE, "Mouse clicked on " + person);
 			return perez;
-			}
-		
-		if(person == "Cam")
-		{
+		}
+
+		if (person == "Cam") {
 			logger.log(Level.FINE, "Mouse clicked on " + person);
 			return jones;
-		}
-		else
+		} else {
 			return null;
+		}
 	}
-
-	public void mouseEntered(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void mouseExited(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	
-	public void mouseReleased(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void mousePressed(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	
-	
-
 }
